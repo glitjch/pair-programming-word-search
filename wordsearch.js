a5806bdd691f5282fa17ea42b7edc29a861bdf8e
glitjch <t.jamesphan@gmail.com>
2022-01-20 03:49:04 +0000

    paired program word search with mocha.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+    const horizontalJoin = letters.map(ls => ls.join(''));
 
-module.exports = wordSearch
+    for (l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+
+    let verticalJoin = [];
+    for (let i = 0; i < letters[0].length; i++) {
+        verticalJoin.push([]);
+        for (let j = 0; j < letters.length; j++) {
+            verticalJoin[i].push(letters[j][i]);    
+        };
+    };
+    
+    verticalJoin = verticalJoin.map(ls => ls.join(''));
+
+    for (l of verticalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    
+    return false;
+};
+
+module.exports = wordSearch;
+
+// pair-programmed with adrian to
